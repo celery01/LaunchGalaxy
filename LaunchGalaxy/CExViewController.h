e8bc96391891f65fe94c8aedbcf71fad24b6cd1e
celery01 <celery01@gmail.com>
2012-03-31 15:42:47 +0800

    Initial Commit

---
+++ LaunchGalaxy/CExViewController.h
@@ -0,0 +1,13 @@
+//
+//  CExViewController.h
+//  LaunchGalaxy
+//
+//  Created by Daniel Lin on 3/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CExViewController : UIViewController
+
+@end
